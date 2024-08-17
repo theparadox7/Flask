83dc31ab1e22e632e06c2470722d943f92e26b7d
theparadox07 <theunknownpradox07@gmail.com>
2024-08-17 11:15:39 +0530

    home page started

--- market.py
+++ market.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hellow_world():
-    return "<h1> Namaste!! </h1>"
+def home_page():
+    return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1> This is about page of {username}</h1>'
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f'<h1> This is about page of {username}</h1>'
+

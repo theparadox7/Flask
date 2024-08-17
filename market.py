ff5fedab745ee9cc80cdbffc43c87e0be0be69a1
theparadox07 <theunknownpradox07@gmail.com>
2024-08-17 16:51:33 +0530

    inheritance nav bar

--- market.py
+++ market.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -15,8 +15,9 @@ def market_page():
 def login_page():
     return render_template('login.html')
 
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
 
-# @app.route('/about/<username>')
-# def about_page(username):
-#     return f'<h1> This is about page of {username}</h1>'
-
+if __name__ == "__main__":
+    app.run(debug=True)

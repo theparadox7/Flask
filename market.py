1b1bbd659d4000c284d979e420453c1232bf1ea4
theparadox07 <theunknownpradox07@gmail.com>
2024-08-17 15:06:00 +0530

    issues

--- market.py
+++ market.py
@@ -3,8 +3,15 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home_page():
     return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
+
+
 
 # @app.route('/about/<username>')
 # def about_page(username):
